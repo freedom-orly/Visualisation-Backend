e296173255409068268307a7cf51af2c08bd73df
kamy-99 <kamyab.rayganshirazinejad@student.nhlstenden.com>
2025-09-24 12:45:02 +0200

    comments

--- sales_prototype.R
+++ sales_prototype.R
@@ -5,7 +5,7 @@ library(ggplot2)
 library(scales)  # make comma() available
 
 
-# Read the "locations" sheet
+# reading .xlsx files
 locations <- read_excel("linktables.xlsx", sheet = "locations")
 # Read the "stores" sheet
 stores <- read_excel("linktables.xlsx", sheet = "stores")
@@ -15,7 +15,7 @@ hours_sample <- read_excel("hours_sample.xlsx", sheet = "hours")
 teams <- read_excel("teams.xlsx", sheet = "Blad1")
 weather <- read_excel("weather.xlsx", sheet = "Blad1")
 
-
+#reading .csv files
 sales <- read.csv("sales_sample2.csv", header = TRUE, sep = ";")
 store <- read.csv("store.csv", header = TRUE, sep = ";")
 subgroup <- read.csv("subgroup.csv", header = TRUE, sep = ";")
@@ -25,9 +25,11 @@ visitor_hourly <- read.csv("visitorhourly_sample2.csv", header = TRUE, sep = ";"
 unique(sales$StoreId)
 unique(store$StoreId)
 
+#store_sort
 all_stores <- merge(store, stores, by = "StoreId")
 all_stores$...4 <- NULL
 
+#merging the data
 all_sales <- merge(all_stores, sales, by = "StoreId")
 all_sales <- merge(all_sales,departments , by ="department_id")
 all_sales <- merge(all_sales, subgroup, by = "SubgroupId")
@@ -54,6 +56,7 @@ all_sales <- all_sales %>%
     everything()  # catches leftover cols like ...4
   )
 
+#convert the transactions to numerics
 all_sales <- all_sales %>%
   mutate(
     NetAmountExcl = gsub(",", ".", NetAmountExcl),  # replace comma with dot
@@ -62,10 +65,12 @@ all_sales <- all_sales %>%
 
 View(all_sales)
 
+#calculate the total sales across the data per store
 total_per_store <- all_sales %>%
   group_by(StoreId) %>%
   summarise(total = sum(NetAmountExcl, na.rm = TRUE))
 
+#geom bar graph for the total sales per store
 ggplot(total_per_store, aes(x = factor(StoreId), y = total)) +
   geom_bar(stat = "identity", fill = "steelblue") +  # create bars
   labs(

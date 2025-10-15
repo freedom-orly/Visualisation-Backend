16f657b2858cf1f6899b7d9befe986f045d5e0e0
mrczarny <j.adamski@zsebydgoszcz.onmicrosoft.com>
2025-10-15 21:07:59 +0200

    get_visualizations wip + scaffolding

--- Handlers/VisualizationHandler.py
+++ Handlers/VisualizationHandler.py
@@ -1,4 +1,5 @@
-from typing import List, jsonify
+from datetime import datetime, timedelta
+from typing import List
 
 from flask_sqlalchemy import SQLAlchemy
 from models.dto_models import ChartDTO, ChartQuery, VisualizationDTO
@@ -12,7 +13,7 @@ db -- SQLAlchemy database session
 Return: 
 ChartDTO object containing the chart data
 """
-def get_chart(query: ChartQuery, db: SQLAlchemy) -> ChartDTO:
+def get_chart(query: ChartQuery, db: SQLAlchemy) -> ChartDTO | None:
     # Implementation of the function to fetch and return visualization data
     #Gets the Visualization from query id
     visual = db.session.get(Visualization, query.id)
@@ -21,12 +22,32 @@ def get_chart(query: ChartQuery, db: SQLAlchemy) -> ChartDTO:
         return None
     
     if query.spread:
-        pass#checks spread
+        return None
+    
+    if query.timespan:
+        return None 
+    
 
-    pass  # Placeholder return
+
+def run_rscript(visualization_id: int, timespan: timedelta, spread: timedelta, db: SQLAlchemy) -> bool:
+    raise NotImplementedError
+
+def get_last_updates(v: int, db: SQLAlchemy) -> List[datetime]:
+    raise NotImplementedError
 
 
 def get_visualizations(db: SQLAlchemy) -> List[VisualizationDTO]:
     dbQuery = db.session.query(Visualization)
-    results = dbQuery.all()
-    return jsonify([results]), 200  
+    query_results: List[Visualization] = dbQuery.all()
+    results: List[VisualizationDTO] = []
+    for v in query_results:
+        last_updates: List[datetime] = get_last_updates(v.id, db) # type: ignore
+        vis_dto = VisualizationDTO(
+            id=v.id, # type: ignore
+            name=v.name, # type: ignore
+            description=v.description, # type: ignore
+            is_prediction=v.prediction, # type: ignore
+            last_updates=last_updates
+        )
+        results.append(vis_dto)
+    return results
